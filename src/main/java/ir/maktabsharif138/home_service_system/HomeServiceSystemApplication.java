1f00b16b1ab71d8082fefabe1e7cb0fe33890fbb
Yeganeh <yeganehshahrabi85@gmail.com>
2026-06-06 09:53:39 +0330

    chore: initialize spring boot project for home service system

---
+++ src/main/java/ir/maktabsharif138/home_service_system/HomeServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif138.home_service_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceSystemApplication.class, args);
+	}
+
+}
